ea3cf48c5201cf5dc34f554db3462597816b313f
DonFuego <matthews.todd@gmail.com>
2020-08-11 21:41:28 -0400

    Added workspace and clarifai api key to .env

--- little_swan_dance_follow_bot-app.py
+++ little_swan_dance_follow_bot-app.py
@@ -4,6 +4,8 @@ load_dotenv()
 from instapy import InstaPy
 from os import getenv
 
+set_workspace(path=getenv('LITTLE_SWAN_DANCE_WORKSPACE'))
+
 session = InstaPy(username=getenv('LITTLE_SWAN_DANCE_USERNAME'), password=getenv('LITTLE_SWAN_DANCE_PASSWORD'), headless_browser=True).login()
 session.set_quota_supervisor(enabled=True, sleep_after=["likes", "comments_d", "follows", "unfollows", "server_calls_h"], sleepyhead=True, stochastic_flow=True, notify_me=False,
                               peak_likes_hourly=20,
