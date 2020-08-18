9fb6c3aa0275019d7457f23926b8217d776aa83a
donfuego <matthews.todd@gmail.com>
2020-08-17 21:23:29 -0400

    Added interactivity of following

--- little_swan_dance_follow_bot.py
+++ little_swan_dance_follow_bot.py
@@ -37,19 +37,20 @@ def job():
         session.set_do_comment(enabled=False)
         session.set_dont_like(["naked", "nsfw"])
         session.set_do_follow(True, percentage=70, times=1)
+        session.set_user_interact(amount=1, randomize=True, percentage=100, media='Photo')
         session.follow_by_tags(['dancestudioownerlife', 'dancestudioowner', 'danceteacher'], amount=30)
         session.follow_user_followers(
             ['twinklestardance', 'confettionthedancefloor', 'toddlerandpreschoolballet', 'studiotogodance',
              'leapnlearn'],
-            amount=20, randomize=True, sleep_delay=30, interact=False)
+            amount=20, randomize=True, sleep_delay=30, interact=True)
         session.follow_likers(
             ['twinklestardance', 'confettionthedancefloor', 'toddlerandpreschoolballet', 'studiotogodance',
              'leapnlearn'],
-            photos_grab_amount=3, follow_likers_per_photo=5, randomize=True, sleep_delay=30, interact=False)
+            photos_grab_amount=3, follow_likers_per_photo=5, randomize=True, sleep_delay=30, interact=True)
         session.follow_commenters(
             ['twinklestardance', 'confettionthedancefloor', 'toddlerandpreschoolballet', 'studiotogodance',
              'leapnlearn'],
-            amount=3, daysold=180, max_pic=20, sleep_delay=30, interact=False)
+            amount=3, daysold=180, max_pic=20, sleep_delay=30, interact=True)
         session.unfollow_users(amount=50, nonFollowers=True, style='RANDOM', unfollow_after=432000, sleep_delay=30)
 
         # session.end()
